29b42e0f724ad4fd50c568e216ffabb67fbc0f8d
James Tickett <jtickett@gmail.com>
2024-09-09 19:11:58 +0100

    sidebar now working with burger menu button. transitions done. sidebar still needs styling though.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,42 @@
 // Stuff to do once the DOM is ready.
 $(document).ready(function(){
 
+    let menuOpen = false;
 
 
-    $('.carousel-slider').slick({
-        //autoplay: true,
-        //autoplaySpeed: 4000,
-        arrows: false,
-        dots: true,
-        adaptiveHeight: true,
-    });
+    // $('.carousel-slider').slick({
+    //     //autoplay: true,
+    //     //autoplaySpeed: 4000,
+    //     arrows: false,
+    //     dots: true,
+    //     adaptiveHeight: true,
+    // });
 
     
     //$(".burger").click(() => ($(".burger").toggleClass("active")));
     //above arrow function does the same, but I find it less readable.
     $(".burger").click(function(){
-        $(".burger").toggleClass("active");
+        setTimeout(() => {
+			$(".burger").toggleClass("active");
+            $("#page-wrapper").toggleClass("sidebar-visible");
+            $("#sidebar").toggleClass("sidebar-visible");
+            menuOpen = true;
+		  }, 50);
+
+          
+        
+    });
+
+    $("#page-wrapper").click(function(){
+
+        if (menuOpen) {
+        setTimeout(() => {
+            $(".burger").removeClass("active");
+            $("#page-wrapper").removeClass("sidebar-visible");
+            $("#sidebar").removeClass("sidebar-visible");
+            menuOpen = false;
+		  }, 50);
+        }
     });
 
   });
