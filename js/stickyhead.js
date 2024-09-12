99a508e9fe22eb8efdfb85a7b8a6fd0d78d2f133
James Tickett <jtickett@gmail.com>
2024-09-12 15:12:12 +0100

    sidebar styling improved (icons and color added)

--- js/stickyhead.js
+++ js/stickyhead.js
@@ -9,15 +9,18 @@ $(document).ready(function(){
         const currentScroll = window.scrollY;
         //  Checking if the user is not scrolling (reached the top).
         if (currentScroll <= 0) {
+            console.log("TOP");
             header.removeClass('sticky-head-down sticky-head-up');
         }
         //  Checking if the user is scrolling down
         else if (currentScroll > scrollLimit && currentScroll > lastScroll) {
+            console.log("DOWN - " + "Current: " + currentScroll + " Last: " + lastScroll);
             header.removeClass('sticky-head-up');
             header.addClass('sticky-head-down');
         }
         //  Checking if the user is scrolling up
         else if (currentScroll < lastScroll){
+            console.log("UP - " + "Current: " + currentScroll + " Last: " + lastScroll);
             header.addClass('sticky-head-up');
             header.removeClass('sticky-head-down');
         }
